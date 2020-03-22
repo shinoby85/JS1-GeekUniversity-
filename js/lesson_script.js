12933a87513834861525ad728f0329df27193ac8
shinoby <ponk@tut.by>
2020-03-22 23:27:53 +0300

    Выполнено задание 1

--- js/lesson_script.js
+++ js/lesson_script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function findTemp(){
+    let tC=document.getElementById('temp');
+    if (!checkTemp(tC.value)) {
+        tC.value="";
+        tC.placeholder='Ошибка. Повторите ввод';
+        return null;
+    }
+    let tF=(9/5)*Number(tC.value)+32;
+    let tempResult=document.getElementById('temp-result');    
+    if(confirm('Данные вывести в alert?')){
+        alert(`Температура ${Number(tC.value)} по Цельсию соответствует температуре ${tF} по Фаренгейту`);
+    }
+    else{
+        tempResult.value=tF;
+        tempResult.style.backgroundColor='lightgreen';
+    }
+    return false;
+}
+
+function checkTemp(temp){
+    if (Number(temp)==isNaN||temp.trim()=='')return false;
+    return true;
+}
+function resetTemp(){
+    let rst=confirm('Очистить форму?');
+    if (rst) {
+        let tempResult=document.getElementById('temp-result');
+        tempResult.style.backgroundColor='white';        
+    }
+    return rst;
+}
