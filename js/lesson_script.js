0704af32cbf6d5f13197530eb4f63a0e4f9449fb
shinoby <ponk@tut.by>
2020-04-05 23:58:46 +0300

    Выполнено второе задание

--- js/lesson_script.js
+++ js/lesson_script.js
@@ -1,20 +1,42 @@
 'use strict';
 
 //Первое задание
-let newWindowBtn=document.getElementById('openWindow');
-let newWindow=document.getElementById('win');
-let exitWindow=document.getElementById('exWin');
+let newWindowBtn = document.getElementById('openWindow');
+let newWindow = document.getElementById('win');
+let exitWindow = document.getElementById('exWin');
 
-newWindowBtn.addEventListener('click',function () {
+newWindowBtn.addEventListener('click', function () {
     newWindow.classList.remove('fadeOut');
     newWindow.classList.remove('hide');
-    newWindow.classList.add('animated','zoomIn');
+    newWindow.classList.add('animated', 'zoomIn');
 
 });
-exitWindow.addEventListener('click',function () {
+exitWindow.addEventListener('click', function () {
     newWindow.classList.remove('zoomIn');
     newWindow.classList.add('fadeOut');
     setTimeout(function () {
         newWindow.classList.add('hide');
-    },1000);
+    }, 1000);
 });
+
+//Второе задание
+let productsBtn = document.getElementsByClassName('next-btn');
+for (let i = 0; i < productsBtn.length; i++) {
+    productsBtn[i].addEventListener('click', function () {
+        let product = this.parentNode;
+        let img=product.querySelector('.img-bird');
+        let text=product.querySelector('.product-text');
+        if(this.name==='btn-on'){
+            this.name='btn-off';
+            this.innerText='Отмена';
+            img.classList.add('hide');
+            text.classList.remove('hide');
+        }
+        else {
+            this.name='btn-on';
+            this.innerText='Подробнее';
+            img.classList.remove('hide');
+            text.classList.add('hide');
+        }
+    });
+}
