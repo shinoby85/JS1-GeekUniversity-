ccee87c235db57d91278b7a27a9d0b5a86c43655
shinoby <ponk@tut.by>
2020-04-05 23:08:47 +0300

    Выполнено задание 1

--- js/lesson_script.js
+++ js/lesson_script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//Первое задание
+let newWindowBtn=document.getElementById('openWindow');
+let newWindow=document.getElementById('win');
+let exitWindow=document.getElementById('exWin');
+
+newWindowBtn.addEventListener('click',function () {
+    newWindow.classList.remove('fadeOut');
+    newWindow.classList.remove('hide');
+    newWindow.classList.add('animated','zoomIn');
+
+});
+exitWindow.addEventListener('click',function () {
+    newWindow.classList.remove('zoomIn');
+    newWindow.classList.add('fadeOut');
+    setTimeout(function () {
+        newWindow.classList.add('hide');
+    },1000);
+});
