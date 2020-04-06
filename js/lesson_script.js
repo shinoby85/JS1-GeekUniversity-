c73ac23f954321e7b54859d4cb6bce465f972297
shinoby <ponk@tut.by>
2020-04-07 00:36:13 +0300

    Формирование шахматной доски

--- js/lesson_script.js
+++ js/lesson_script.js
@@ -40,3 +40,107 @@ for (let i = 0; i < productsBtn.length; i++) {
         }
     });
 }
+
+//Третье задание
+let btnBoard=document.getElementsByClassName('board-btn')[0];
+let massLetters=['A','B','C','D','E','F','G','H'];
+
+/**
+ * Функция формирует шахматную доску
+ */
+let createBoard=function () {
+    this.style.display='none';
+    let parentBlock=this.parentNode;
+    let board=document.createElement('div');
+    board.classList.add('full-board');
+    parentBlock.appendChild(board);
+    createLetters(board);
+    createNumbers(board);
+    let field=document.createElement('div');
+    field.classList.add('field');
+    board.appendChild(field);
+    createCell(field);
+
+};
+
+/**
+ * Размещает на доске буквы
+ * @param board родительский элемент
+ */
+function createLetters(board){
+    for(let i=1;i<9;i++){
+        let numBlock=document.createElement('div');
+        numBlock.innerText=massLetters[i-1];
+        numBlock.classList.add(`num-0${i}`);
+        board.appendChild(numBlock);
+    }
+
+}
+
+/**
+ * Размещает на шахматной доске цифры
+ * @param board родительский элемент
+ */
+function createNumbers(board){
+    for(let i=1;i<9;i++){
+        let numBlock=document.createElement('div');
+        numBlock.innerText=i;
+        numBlock.classList.add(`num-${i}0`);
+        board.appendChild(numBlock);
+    }
+}
+
+/**
+ * Добавление клеток на поле
+ * @param field родительский класс
+ */
+function createCell(field){
+    for (let i = 1; i < 9; i++) {
+        for (let j = 1; j < 9; j++) {
+            let cell=document.createElement('div');
+            cell.id=`cell-${i}${j}`;
+            if ((i%2===0 && j%2!==0)||(i%2!==0 && j%2===0)){
+                cell.classList.add('cellBlack');
+            }
+            field.appendChild(cell);
+        }
+    }
+}
+
+
+btnBoard.addEventListener('click',createBoard);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
